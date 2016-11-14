8bca689326e66337570d1d8e80cc757089091c55
John Hearn <jhearn@gmail.com>
2016-11-14 10:10:41 -0800

    new project

---
+++ JohnHearn/Objective-C_Demo/Objective-C_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objective-C_Demo
+//
+//  Created by John D Hearn on 11/14/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
