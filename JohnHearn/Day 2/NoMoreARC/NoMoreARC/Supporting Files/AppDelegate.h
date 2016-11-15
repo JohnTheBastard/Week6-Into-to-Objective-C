74635f28e24a6aee691f1f763e13b6494034c0a6
John Hearn <jhearn@gmail.com>
2016-11-15 12:04:53 -0800

    initial commit

---
+++ JohnHearn/Day 2/NoMoreARC/NoMoreARC/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoMoreARC
+//
+//  Created by John D Hearn on 11/15/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
