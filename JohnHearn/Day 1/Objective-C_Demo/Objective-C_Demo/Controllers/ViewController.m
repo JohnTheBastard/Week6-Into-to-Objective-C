1002afdf641c7004129435be007297be3ee141d0
John Hearn <jhearn@gmail.com>
2016-11-15 11:47:09 -0800

    in-class demo code (before creating new project with ARC turned off)

--- JohnHearn/Day 1/Objective-C_Demo/Objective-C_Demo/Controllers/ViewController.m
+++ JohnHearn/Day 1/Objective-C_Demo/Objective-C_Demo/Controllers/ViewController.m
@@ -69,9 +69,64 @@ extern void printNames(NSMutableArray *people);
 
     NSArray *wordArray = [sentence asWordArray];
 
+
     for(NSString *word in wordArray){
         NSLog(@"%@", word);
     }
+
+    NSNumber *number1 =@123;
+    NSNumber *number2 =@321;
+    if([number1 isEqualToNumber:number2]) {
+
+    }
+
+    switch ([number1 compare:number2]) {
+        case NSOrderedSame:
+            NSLog(@"Values are equal");
+            break;
+        case NSOrderedAscending:
+            NSLog(@"Values are ascending");
+            break;
+        case NSOrderedDescending:
+            NSLog(@"Values are descending");
+            break;
+    }
+
+    //NSString *string = [NSString stringWithFormat:@"%@, %@", number1, number2];
+
+    NSString *str1 = @"one";
+    NSString *str2 = @"two";
+    NSString *str3 = @"three";
+    NSNumber *number = @55;
+    NSArray *array = [NSArray arrayWithObjects:str1, str2, str3, number, nil];
+    NSArray *array2 = @[str1, str2, str3, number];
+
+    if ( [array isEqualToArray:array2] ){
+        NSLog(@"true");
+    }
+
+    NSDictionary *dictionary = @{@"str1":str1, @2:str2, @"str3":str3};
+
+    NSString *first = [dictionary objectForKey:@"str1"];
+    NSString *second = dictionary[@2];
+
+    NSMutableDictionary *mutDict = [[NSMutableDictionary alloc] init];
+    [mutDict setObject:number forKey:@2];
+
+//  @{@"str1":str1, @2:str2, @"str3":str3};
+//
+//    NSString *first = [dictionary objectForKey:@"str1"];
+//    NSString *second = dictionary[@2];
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    NSArray *urls = [fileManager URLsForDirectory:NSDocumentDirectory
+                                        inDomains:NSUserDomainMask];
+
+    NSLog(@"%@", urls);
+
+    
+
 
 
 }
